b9edccc038d70d25e6b1c723c6bae7831f0fa870
Elías DC <143301482+EDC050805@users.noreply.github.com>
2025-06-20 10:50:33 -0500

    Add files via upload

---
+++ T3/src/t3/AdaptadorGuardar.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t3;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Estudiante
+ */
+public class AdaptadorGuardar implements MouseListener{
+    private JFrame padre;
+    private String mensaje;
+
+    public AdaptadorGuardar(JFrame padre,String mensaje) {
+        this.padre = padre;
+        this.mensaje = mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        JOptionPane.showMessageDialog(this.padre, this.mensaje);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+}
---
+++ T3/src/t3/T3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t3;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class T3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GuardarConsulta consulta =  new GuardarConsulta();
+    }
+    
+}
